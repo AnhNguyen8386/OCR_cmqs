664276c99e397545461bd2690110ebe41e5448d0
AnhNguyen8386 <anhnguyen.29599@gmail.com>
2025-06-14 21:50:16 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 from io import BytesIO
-from app.ocr_pipeline import process_image
+from ocr_pipeline import process_image
 app = FastAPI()
 @app.post("/ocr/")
 def ocr_api(file: UploadFile = File(...)):
