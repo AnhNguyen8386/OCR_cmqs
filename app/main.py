16f13ce3ad169022525734a96443304486cf1fa2
AnhNguyen8386 <anhnguyen.29599@gmail.com>
2025-06-15 00:24:08 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -22,3 +22,6 @@ def ocr_api(file: UploadFile = File(...)):
         "status": "success",
         "data": result
     }
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
